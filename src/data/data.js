c8f172e9683e46e67a8a90355b5eac8a2b02ae2a
Gauthier LO <lo.gauthier.glo@gmail.com>
2025-02-14 17:35:14 +0100

    some logic

---
+++ src/data/data.js
@@ -0,0 +1,8 @@
+export const gameState = {
+  ON_GOING: "on_going",
+  ENDED: "Match already finished",
+  DRAW: "draw",
+  NOT_FOUND: "not found",
+  NOT_LAST: "not last",
+  ALREADY_MOVED: "move already given",
+}
